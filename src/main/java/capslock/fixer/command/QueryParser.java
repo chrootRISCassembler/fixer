434f8d03f014494277cac9f7d2e845c6e8415fa8
RISCassembler <coffee5.56natosystemcall@gmail.com>
2018-04-09 07:18:50 +0900

    dev class QueryParser

---
+++ src/main/java/capslock/fixer/command/QueryParser.java
@@ -0,0 +1,11 @@
+package capslock.fixer.command;
+
+class QueryParser {
+    /**
+     *
+     * @param query パース対象のクエリ文字列
+     * @throws IllegalArgumentException 引数のクエリが不正
+     */
+    QueryParser(String query) throws IllegalArgumentException{
+    }
+}
