d8fdb15926203f7b65867d3d5b40f837fcbf4d6b
RISCassembler <coffee5.56natosystemcall@gmail.com>
2018-04-09 14:36:22 +0900

    Commit command puts out message

--- src/main/java/capslock/fixer/command/Commit.java
+++ src/main/java/capslock/fixer/command/Commit.java
@@ -9,8 +9,10 @@ public class Commit extends Command {
     public boolean run(String line) {
         try {
             final var writer = new JSONDBWriter(jsonDBFile);
+            System.out.println("ファイルへの書き込みを開始します...");
             gameList.forEach(writer::add);
             writer.flush();
+            System.out.println(gameList.size() + "件のゲーム情報を書き込みました.");
             return true;
         }catch (IOException ex){
             System.err.println("ファイル書き出しに失敗しました.");
